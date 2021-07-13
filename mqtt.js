074b90482539d346d3c5fc19816c44635e41e324
Selafina0821 <83298971+Selafina0821@users.noreply.github.com>
2021-07-13 14:41:27 +0800

    Add files via upload

--- mqtt.js
+++ mqtt.js
@@ -1,25 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 var mqtt = require('mqtt');
 var opt = {
   port:1883,
-  clientId: 'nodejs'
 };
-var io = require("socket.io");
-var express = require("express");
-var app = express();
-app.use(express.static('www'));
-var server = app.listen(5438);
-
 var client  = mqtt.connect('mqtt://120.126.16.88',opt);
-var sio = io(server);
-
 client.on('connect', function () {
   console.log('已連接至MQTT伺服器');
   client.subscribe("nodejs");
 });
 
-sio.on('connection', function(socket){
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('front-end connected');
   client.on('message', function (topic, msg) { 
-      console.log('收到 ' + topic + ' 主題，訊息：' + msg.toString());
-      socket.emit('mqtt', { 'msg': msg.toString() });
-  });
+    console.log('收到 ' + topic + ' 主題，訊息：' + msg.toString());
+    socket.emit('mqtt', { 'msg': msg.toString() });
+});
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
