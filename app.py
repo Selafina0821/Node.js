9606e1ce31a64177dea6362ea61266b525196851
Selafina0821 <83298971+Selafina0821@users.noreply.github.com>
2022-08-23 10:44:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ line_bot_api.push_message('Ub84e35c61ef80aac1f9e2519feba84d5', TextSendMessage(t
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-# get X-Line-Signature header value
-signature = request.headers['X-Line-Signature']
-# get request body as text
-body = request.get_data(as_text=True)
-app.logger.info("Request body: " + body)
+ # get X-Line-Signature header value
+ signature = request.headers['X-Line-Signature']
+ # get request body as text
+ body = request.get_data(as_text=True)
+ app.logger.info("Request body: " + body)
 
 # handle webhook body
 try:
